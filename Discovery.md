a389100f1a94c46a88ac7c7c06970440f769f78b
tucommenceapousser <129875733+tucommenceapousser@users.noreply.github.com>
2023-08-03 10:06:30 +0000

    deta

---
+++ Discovery.md
@@ -0,0 +1,6 @@
+---
+app_name: trknxor
+tagline: generate a php webshell encoded in xor
+---
+
+
--- replit.nix
+++ replit.nix
@@ -1,5 +1,6 @@
 { pkgs }: {
   deps = [
+    pkgs.unzip
     pkgs.python310Full
     pkgs.replitPackages.prybar-python310
     pkgs.replitPackages.stderred
